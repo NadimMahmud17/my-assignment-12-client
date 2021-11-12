e531024bc22015925130904e2e4d981ce25e4913
NadimMahmud17 <nadim.bd62@gmail.com>
2021-11-12 18:50:00 +0600

    first commit

---
+++ src/Firebase/Firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAJXHkE8kcBZUSxYB6NyneY9Fnnc5-Kjk0",
+  authDomain: "tesla-car-shop.firebaseapp.com",
+  projectId: "tesla-car-shop",
+  storageBucket: "tesla-car-shop.appspot.com",
+  messagingSenderId: "52846110847",
+  appId: "1:52846110847:web:7265355ba2e9008048f74a"
+};
+export default firebaseConfig;
